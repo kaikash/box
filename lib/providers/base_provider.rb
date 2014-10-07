f7d2d40196075896a20e2421615d7b177c8af901
Gleb Vishnevsky <vishgleb@mail.ru>
2014-10-07 15:29:51 +0400

    naidenaya L'vom oshibka

--- lib/providers/base_provider.rb
+++ lib/providers/base_provider.rb
@@ -1,7 +1,7 @@
-# require "#{Rails.root}/lib/providers/api_provider"
+require "#{Rails.root}/lib/providers/api_provider"
+
 module Provider
 	class Base
-		include Provider::API
 		def call(action, params)
 			return {error:{error_msg: "Missing line", error_code: 1488}}.to_json if(!params[:line])
 			method(action.to_s + "_line").call(self.class.name.split("::")[1].downcase.to_sym, params[:line])
@@ -12,16 +12,16 @@ module Provider
 				when :vk
 					params = {attachments: [], access_token: @access_token, storage_id: @storage_id, message: line.name}
 					if line.img
-            photo_path = Provider::API.load_img(:vk, line.img)
+            photo_path = ::Provider::API.load_img(:vk, line.img)
 						params[:attachments] = img_path
 					end
-					res = Provider::API.query(:vk, "wall.post", params)
+					res = ::Provider::API.query(:vk, "wall.post", params)
 				when :fb
           params = {access_token: @access_token, storage_id: @storage_id, message: line.name, type: :feed}
           if line.img
             params[:type] = photos
           end
-          res = Provider::API.query(:fb, :post, params)
+          res = ::Provider::API.query(:fb, :post, params)
 			end
 			res
 		end
@@ -42,7 +42,7 @@ module Provider
 				when :fb
 					:delete
 				end
-				Provider::API.query(provider, method, params)
+				::Provider::API.query(provider, method, params)
 		end
 	end
 end
